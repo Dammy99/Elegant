65ae94dbb9e2af78386f2e723efa5db046fd684e
Dammy99 <deslyly.prod@gmail.com>
2023-06-26 13:43:29 +0300

    Ended services page

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -2,8 +2,37 @@ import React from "react";
 import styles from "../../app/style.module.css";
 const Header = () => {
   return (
-    <div className={styles.services}>
-      <div>123</div>
+    <div className={styles.servicespage}>
+      <div className={styles.servicespage__hairbox}>
+        <img src="../../../public/services_hair.png"></img>
+        <p className={styles.servicespage__text}>Перукарські послуги</p>
+        <button
+          className={`${styles.servicespage__hairbox} ${styles.servicespage__sender}`}
+        >
+          <p>ДЕТАЛЬНІШЕ</p>
+        </button>
+      </div>
+      <div className={styles.servicespage__hairbox}>
+        <img src="../../../public/manicure_services.jpg"></img>
+        <p className={styles.servicespage__text}>Послуги манікюру</p>
+        <button
+          className={`${styles.servicespage__hairbox} ${styles.servicespage__sender}`}
+        >
+          <p>ДЕТАЛЬНІШЕ</p>
+        </button>
+      </div>
+      <div className={styles.servicespage__hairbox}>
+        <img
+          className={styles.servicespage__hairbox_flip}
+          src="../../../public/brows_services.png"
+        ></img>
+        <p className={styles.servicespage__text}>Брови та вії</p>
+        <button
+          className={`${styles.servicespage__hairbox} ${styles.servicespage__sender}`}
+        >
+          <p>ДЕТАЛЬНІШЕ</p>
+        </button>
+      </div>
     </div>
   );
 };
